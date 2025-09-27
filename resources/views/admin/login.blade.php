ac55bb5e47d71beda9823f5b283e18667610e23d
indramahayana <indramahayana3@gmail.com>
2025-09-27 16:51:31 +0800

    Login page

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,20 +1,40 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login Admin</title>
-</head>
-<body>
-    <h1>Login Admin</h1>
+@extends('layouts.admin')
 
-    @if($errors->any())
-        <p style="color:red">{{ $errors->first('login') }}</p>
-    @endif
-
-    <form method="POST" action="{{ route('admin.login.post') }}">
-        @csrf
-        <p>Email: <input type="email" name="email" required></p>
-        <p>Password: <input type="password" name="password" required></p>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>
+@section('content')
+    <section class="bg-gray-50">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <a href="#" class="flex items-center mb-6 text-2xl font-bold uppercase text-gray-900">
+                <img class="w-8 mr-2" src="/images/gemilanglogo.png" alt="logo">
+                Gemilang Meubel
+            </a>
+            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    @if ($errors->any())
+                        <p style="color:red">{{ $errors->first('login') }}</p>
+                    @endif
+                    <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                        Admin Sign in
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" method="POST" action="{{ route('admin.login.post') }}">
+                        @csrf
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
+                            <input type="email" name="email" id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                                placeholder="name@company.com" required="">
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                            <input type="password" name="password" id="password" placeholder="••••••••"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                required="">
+                        </div>
+                        <button type="submit"
+                            class="w-full bg-gray-800 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer">Sign
+                            in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
